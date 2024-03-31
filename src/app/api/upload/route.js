3e4912d7fde015d1f9f367815d8b1b334ba7e3ba
eurorondon03 <eurorondon03@gmail.com>
2024-03-31 09:05:25 -0400

    seguimos con prueba

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -8,8 +8,8 @@ cloudinary.config({
 });
 
 export async function POST(request) {
-  // const data = await request.formData();
-  // const image = data.get("file");
+  const data = await request.formData();
+  const image = data.get("file");
 
   // if (!image) {
   //   return NextResponse.json("no se ha encontrado ninguna imagen", {
@@ -17,24 +17,24 @@ export async function POST(request) {
   //   });
   // }
 
-  // const bytes = await image.arrayBuffer();
-  // const buffer = Buffer.from(bytes);
+  const bytes = await image.arrayBuffer();
+  const buffer = Buffer.from(bytes);
 
-  // const response = await new Promise((resolve, reject) => {
-  //   cloudinary.uploader
-  //     .upload_stream({ folder: "Next.js" }, (err, result) => {
-  //       if (err) {
-  //         reject(err);
-  //       }
-  //       resolve(result);
-  //     })
-  //     .end(buffer);
-  // });
+  const response = await new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream({ folder: "Next.js" }, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      })
+      .end(buffer);
+  });
 
-  // return NextResponse.json({
-  //   message: "imagen subida",
-  //   data: response,
-  // });
-  console.log("prueba");
-  return NextResponse.json("Prueba");
+  return NextResponse.json({
+    message: "imagen subida",
+    data: response,
+  });
+  // console.log("recibiendo imagen");
+  // return NextResponse.json("recibiendo image", image);
 }
