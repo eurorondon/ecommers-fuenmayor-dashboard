62f77dd59502f1f2f8699c40c25dc3d020b3c2ba
eurorondon03 <eurorondon03@gmail.com>
2024-03-31 13:42:34 -0400

    change api newcategory

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "ecommersfuenmayorcli": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
--- src/app/add-product/page.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UpdateProduct from "../components/UpdateProduct";
-
-function page() {
-  return <UpdateProduct />;
-}
-
-export default page;
