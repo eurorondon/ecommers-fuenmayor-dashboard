6df9fc4d79e65bd430943587e3f202fecf28e58d
eurorondon03 <eurorondon03@gmail.com>
2024-03-30 22:00:23 -0400

    testing aws files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ecommersfuenmayorcli": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
